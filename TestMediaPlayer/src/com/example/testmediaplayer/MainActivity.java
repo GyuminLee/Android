cecf66f34c786dfb1705b7b8d36df3f0492a75bc
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-01-20 11:07:23 +0000

    Search Media

--- TestMediaPlayer/src/com/example/testmediaplayer/MainActivity.java
+++ TestMediaPlayer/src/com/example/testmediaplayer/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.testmediaplayer;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
@@ -17,11 +21,17 @@ public class MainActivity extends Activity {
 	MediaPlayer mPlayer;
 	Handler mHandler = new Handler();
 	SeekBar mSeekBar;
+	SeekBar mVolumeBar;
+	
 	boolean isStopped = false;
 	boolean isSeekChanged = false;
+	AudioManager mAudioManager;
+	
 	
 	int mPosition = -1;
 	final static int UPDATE_INTERVAL = 200;
+	
+	final static int REQUEST_CODE_SEARCH_MEDIA = 0;
 	
 	Runnable updateRunnable = new Runnable() {
 
@@ -131,13 +141,89 @@ public class MainActivity extends Activity {
 		});
 		
 		
-		mPlayer = MediaPlayer.create(this, R.raw.winter_blues);
+//		mPlayer = MediaPlayer.create(this, R.raw.winter_blues);
+
+		mPlayer = new MediaPlayer();
+		
+		mVolumeBar = (SeekBar)findViewById(R.id.seekBar2);
+		
+		mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		int volumeMax = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		mVolumeBar.setMax(volumeMax);
+		mVolumeBar.setProgress(currentVolume);
+		
+		mVolumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
 				
-		int max = mPlayer.getDuration();
-		mSeekBar.setMax(max);
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				if (fromUser) {
+					mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+				}
+			}
+		});
+		
+		btn = (Button)findViewById(R.id.button4);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(MainActivity.this, MediaSearchActivity.class);
+				startActivityForResult(i, REQUEST_CODE_SEARCH_MEDIA);
+			}
+		});
+		
 		
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		if (requestCode == REQUEST_CODE_SEARCH_MEDIA) {
+			if (resultCode == RESULT_OK) {
+				Uri mediaUri = data.getData();
+				mPlayer.reset();
+				try {
+					mPlayer.setDataSource(this, mediaUri);
+					mPlayer.prepare();
+					int max = mPlayer.getDuration();
+					mSeekBar.setMax(max);
+					mSeekBar.setProgress(0);					
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SecurityException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
