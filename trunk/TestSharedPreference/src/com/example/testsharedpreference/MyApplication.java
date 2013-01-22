2f483fc920de2a8e79ebcb16fc3834fd10da12f0
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-01-22 01:37:23 +0000

    SharedPreference

---
+++ trunk/TestSharedPreference/src/com/example/testsharedpreference/MyApplication.java
@@ -0,0 +1,20 @@
+package com.example.testsharedpreference;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+
+	static Context context;
+	
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		context = this;
+	}
+	
+	public static Context getContext() {
+		return context;
+	}
+}
