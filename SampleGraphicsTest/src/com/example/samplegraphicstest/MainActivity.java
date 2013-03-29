c8544fa67bbe1dbeaafd10142f8cbbbbb907af5a
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-03-29 07:53:44 +0000

    View update

--- SampleGraphicsTest/src/com/example/samplegraphicstest/MainActivity.java
+++ SampleGraphicsTest/src/com/example/samplegraphicstest/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.samplegraphicstest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	MyView mView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(new MyView(this));
+		setContentView(R.layout.activity_main);
+		mView = (MyView)findViewById(R.id.myView);
+		Button btn = (Button)findViewById(R.id.button1);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mView.setBGColor(Color.GRAY);
+			}
+		});
 	}
 
 	@Override
