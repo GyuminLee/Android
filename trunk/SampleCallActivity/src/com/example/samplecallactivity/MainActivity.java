5ab62a92f32f2ed44ab831c1c38ea9ce66b68621
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-01 08:00:39 +0000

    Call

---
+++ trunk/SampleCallActivity/src/com/example/samplecallactivity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.samplecallactivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button btn = (Button)findViewById(R.id.button1);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(Intent.ACTION_VIEW,
+						Uri.parse("qwer://www.google.com/"));
+				startActivity(i);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
