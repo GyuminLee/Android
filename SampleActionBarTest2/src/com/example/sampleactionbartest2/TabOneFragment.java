dd6f2339b192bb45845d8d4ccb2b10c4a03ea393
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-03 08:32:37 +0000

    Actionbar

---
+++ SampleActionBarTest2/src/com/example/sampleactionbartest2/TabOneFragment.java
@@ -0,0 +1,21 @@
+package com.example.sampleactionbartest2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TabOneFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View v = inflater.inflate(R.layout.tab_layout, container, false);
+		TextView tv = (TextView)v.findViewById(R.id.textView1);
+		tv.setText("TabOneFragment");
+		return v;
+	}
+}
