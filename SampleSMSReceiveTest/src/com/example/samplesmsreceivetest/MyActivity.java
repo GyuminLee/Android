a3ed194a46923b4501fe441cac0558b80a53d15a
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-08 06:44:56 +0000

    Receiver

---
+++ SampleSMSReceiveTest/src/com/example/samplesmsreceivetest/MyActivity.java
@@ -0,0 +1,17 @@
+package com.example.samplesmsreceivetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	    setContentView(R.layout.my_activity);
+	}
+
+}
