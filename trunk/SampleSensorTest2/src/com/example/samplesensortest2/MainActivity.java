d4ab258b1b0077c587354649df4caddf2c98fef8
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-12 04:39:33 +0000

    Compass View

--- trunk/SampleSensorTest2/src/com/example/samplesensortest2/MainActivity.java
+++ trunk/SampleSensorTest2/src/com/example/samplesensortest2/MainActivity.java
@@ -16,6 +16,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 	SensorManager mSensorManager;
 	float[] accValues = new float[3];
 	float[] magValues = new float[3];
+
+	float[] Rm = new float[9];
+	float[] Im = new float[9];
+
+	float[] orientation = new float[3];
+	
 	float proximityMeter;
 	long proximityTime;
 	
@@ -28,12 +34,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 	
 	Handler mHandler = new Handler();
 	
+	CompassView compassView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-		
+		compassView = (CompassView)findViewById(R.id.compassView);
 	}
 	
 	
@@ -44,8 +52,9 @@ public class MainActivity extends Activity implements SensorEventListener {
 		mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_GAME);
 		Sensor magSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 		mSensorManager.registerListener(this, magSensor, SensorManager.SENSOR_DELAY_GAME);
-		Sensor proxiSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-		mSensorManager.registerListener(this, proxiSensor, SensorManager.SENSOR_DELAY_GAME);
+//		Sensor proxiSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+//		mSensorManager.registerListener(this, proxiSensor, SensorManager.SENSOR_DELAY_GAME);
+		mHandler.post(drawRunnable);
 		super.onStart();
 	}
 	
@@ -53,6 +62,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		mSensorManager.unregisterListener(this);
+		mHandler.removeCallbacks(drawRunnable);
 		super.onStop();
 	}
 
@@ -77,32 +87,45 @@ public class MainActivity extends Activity implements SensorEventListener {
 			accValues[0] = event.values[0];
 			accValues[1] = event.values[1];
 			accValues[2] = event.values[2];
-			float acc = (float)Math.sqrt(accValues[0] * accValues[0] + accValues[1] * accValues[1] + accValues[2] * accValues[2]);
-			if (Math.abs(acc - oldAcc) > LIMIT) {
-				mCount++;
-				mHandler.removeCallbacks(resetCount);
-				if (mCount > SHAKE_COUNT) {
-					// shake...
-					mCount = 0;
-					Toast.makeText(this, "Shaking...", Toast.LENGTH_SHORT).show();
-				} else {
-					mHandler.postDelayed(resetCount, 2000);
-				}
-			}
-			oldAcc = acc;
+//			float acc = (float)Math.sqrt(accValues[0] * accValues[0] + accValues[1] * accValues[1] + accValues[2] * accValues[2]);
+//			if (Math.abs(acc - oldAcc) > LIMIT) {
+//				mCount++;
+//				mHandler.removeCallbacks(resetCount);
+//				if (mCount > SHAKE_COUNT) {
+//					// shake...
+//					mCount = 0;
+//					Toast.makeText(this, "Shaking...", Toast.LENGTH_SHORT).show();
+//				} else {
+//					mHandler.postDelayed(resetCount, 2000);
+//				}
+//			}
+//			oldAcc = acc;
 			break;
 		case Sensor.TYPE_MAGNETIC_FIELD :
 			magValues[0] = event.values[0];
 			magValues[1] = event.values[1];
 			magValues[2] = event.values[2];
 			break;
-		case Sensor.TYPE_PROXIMITY :
-			proximityMeter = event.values[0];
-			proximityTime = event.timestamp;
-			break;
+//		case Sensor.TYPE_PROXIMITY :
+//			proximityMeter = event.values[0];
+//			proximityTime = event.timestamp;
+//			break;
 		}
 		
+		SensorManager.getRotationMatrix(Rm, Im, accValues, magValues);
+		SensorManager.getOrientation(Rm, orientation);
 	}
+	
+	Runnable drawRunnable = new Runnable() {
+		
+		@Override
+		public void run() {
+			compassView.setOrientation((float)Math.toDegrees(orientation[0]));
+			mHandler.postDelayed(this, 100);
+		}
+	};
+	
+	
 	
 	Runnable resetCount = new Runnable() {
 		public void run() {
