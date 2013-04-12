9df6b62f37e6bc046961c8f0741377e059938db5
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-12 05:27:51 +0000

    Notification Compat

---
+++ trunk/SampleNotificationCompactTest/src/com/example/samplenotificationcompacttest/MyActivity1.java
@@ -0,0 +1,17 @@
+package com.example.samplenotificationcompacttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity1 extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	    setTitle("MyActivity1");
+	}
+
+}
