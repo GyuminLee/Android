3a79e5957bb7b5ad75ccbb5d76ffa83d78050891
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-12 06:16:21 +0000

    Alarm

---
+++ SampleAlarmTest/src/com/example/samplealarmtest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.samplealarmtest;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button btn = (Button)findViewById(R.id.button1);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+				Intent i = new Intent(MainActivity.this, AlarmActivity.class);
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0, i, 0);
+				am.set(AlarmManager.RTC, System.currentTimeMillis() + 10  * 1000, pi);				
+			}
+		});
+		
+		btn = (Button)findViewById(R.id.button2);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(MainActivity.this, AlarmActivity.class);
+				startActivity(i);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
