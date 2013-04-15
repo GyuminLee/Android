51e8f1d19ebcccb1901983fec6778eeb040f6fa1
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-04-15 03:16:47 +0000

    gson commit

---
+++ HelloNetwork/src/com/example/hellonetwork/parser/GsonResultParser.java
@@ -0,0 +1,37 @@
+package com.example.hellonetwork.parser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.google.gson.Gson;
+
+public class GsonResultParser<T> extends InputStreamParser {
+
+	T result;
+	Class clazz;
+	
+	public GsonResultParser(Class clazz) {
+		this.clazz = clazz;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void doParse(InputStream is) throws InputStreamParserException {
+		// TODO Auto-generated method stub
+		InputStreamReader isr = new InputStreamReader(is);
+		result = (T) getGson().fromJson(isr,clazz);
+	}
+	
+	
+
+	protected Gson getGson() {
+		return new Gson();
+	}
+
+	@Override
+	public Object getResult() {
+		// TODO Auto-generated method stub
+		return result;
+	}
+
+}
