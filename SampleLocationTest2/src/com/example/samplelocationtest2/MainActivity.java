6f87b2b67bd31fa9565d9f0e8f969183aa694d40
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-05-13 06:00:33 +0000

    Alert

--- SampleLocationTest2/src/com/example/samplelocationtest2/MainActivity.java
+++ SampleLocationTest2/src/com/example/samplelocationtest2/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.samplelocationtest2;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	LocationManager mLocationManager;
 	String mProvider;
+	EditText keywordView;
+	ListView list;
+	ArrayAdapter<Address> mAdapter;
 	
 	LocationListener mListener = new LocationListener() {
 		
@@ -70,7 +83,49 @@ public class MainActivity extends Activity {
 				mLocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, pi);
 			}
 		});
+		keywordView = (EditText)findViewById(R.id.editText1);
+		list = (ListView)findViewById(R.id.listView1);
 		
+		btn = (Button)findViewById(R.id.search);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (Geocoder.isPresent()) {
+					Geocoder geocoder = new Geocoder(MainActivity.this, Locale.KOREA);
+					try {
+						List<Address> addresses = geocoder.getFromLocationName(keywordView.getText().toString(), 10);
+						mAdapter = new ArrayAdapter<Address>(MainActivity.this, android.R.layout.simple_list_item_1, addresses);
+						list.setAdapter(mAdapter);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Address address = mAdapter.getItem(position);
+				Location location = new Location("MyAddressLocation");
+				location.setLatitude(address.getLatitude());
+				location.setLongitude(address.getLongitude());
+				
+				Intent i = new Intent(MainActivity.this, MyAlertService.class);
+				i.putExtra(MyAlertService.PARAM_ALERT_LOCATION, location);
+				i.putExtra(MyAlertService.PARAM_ALERT_ADDRESS, address);
+				PendingIntent pi = PendingIntent.getService(MainActivity.this, 0, i, 0);
+				mLocationManager.addProximityAlert(location.getLatitude(), 
+						location.getLongitude(), 50, -1, pi);
+				Toast.makeText(MainActivity.this, "ProximityAlert lat : " + location.getLatitude() + ", lng : " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+			}
+		});
 		
 	}
 	
--- SampleLocationTest2/src/com/example/samplelocationtest2/MyLocationService.java
+++ SampleLocationTest2/src/com/example/samplelocationtest2/MyLocationService.java
@@ -1,6 +1,8 @@
 package com.example.samplelocationtest2;
 
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
@@ -21,6 +23,11 @@ public class MyLocationService extends Service {
 			Toast.makeText(this, 
 					"Location lat : " + location.getLatitude() + ", lng : " + location.getLongitude(), 
 					Toast.LENGTH_SHORT).show();
+			LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+			Intent i = new Intent(this,MyAlertService.class);
+			i.putExtra(MyAlertService.PARAM_ALERT_LOCATION, location);
+			PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+			locationManager.addProximityAlert(location.getLatitude(), location.getLongitude(), 50, -1, pi);
 		}
 		return Service.START_NOT_STICKY;
 	}
