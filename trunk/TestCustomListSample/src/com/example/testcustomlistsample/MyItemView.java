ae18c214e4f50f8a3bfcf34b6cfee2a507d7daa8
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-06-04 06:38:49 +0000

    Listener

--- trunk/TestCustomListSample/src/com/example/testcustomlistsample/MyItemView.java
+++ trunk/TestCustomListSample/src/com/example/testcustomlistsample/MyItemView.java
@@ -3,6 +3,7 @@ package com.example.testcustomlistsample;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,11 +16,32 @@ public class MyItemView extends FrameLayout {
 	MyData mData;
 	Drawable happy, neutral, sad;
 	
+	OnItemImageClickListener mListener;
+	
+	public interface OnItemImageClickListener {
+		public void onItemImageClick(MyData data);
+	}
+	
+	public void setOnItemImageClickListener(OnItemImageClickListener listener) {
+		mListener = listener;
+	}
+	
 	public MyItemView(Context context) {
 		super(context);
 		LayoutInflater inflater = LayoutInflater.from(context);
 		inflater.inflate(R.layout.item_layout, this);
 		iconView = (ImageView)findViewById(R.id.imageView1);
+		iconView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (mListener != null) {
+					mListener.onItemImageClick(mData);
+				}
+			}
+		});
+		
 		nameView = (TextView)findViewById(R.id.textView1);
 		descView = (TextView)findViewById(R.id.textView2);
 		happy = context.getResources().getDrawable(R.drawable.stat_happy);
