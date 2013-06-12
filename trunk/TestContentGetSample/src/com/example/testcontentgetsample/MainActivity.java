3101f6a6855b7da0abf9321e2ef4000fd68e6798
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-06-12 07:19:48 +0000

    Filter

--- trunk/TestContentGetSample/src/com/example/testcontentgetsample/MainActivity.java
+++ trunk/TestContentGetSample/src/com/example/testcontentgetsample/MainActivity.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -29,27 +31,57 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				ContentResolver resolver = getContentResolver();
-				Uri contactUrl = ContactsContract.Contacts.CONTENT_URI;
-				String[] columns = { ContactsContract.Contacts._ID,
-						ContactsContract.Contacts.DISPLAY_NAME };
-				String selection = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
-			            + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
-				String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
-				String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
-				int[] to = {android.R.id.text1};
-				
-				Cursor c = resolver.query(contactUrl, columns, selection, null, sortOrder);
-				SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-						MainActivity.this, 
-						android.R.layout.simple_list_item_1, 
-						c, 
-						from, 
-						to, 
-						0);
-				list.setAdapter(adapter);
+				String keyword = keywordView.getText().toString();
+				showList(keyword);
 			}
 		});
+		keywordView.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				showList(s.toString());
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				
+			}
+		});
+	}
+	
+	
+	private void showList(String keyword) {
+		ContentResolver resolver = getContentResolver();
+		
+		Uri contactUrl = ContactsContract.Contacts.CONTENT_URI;
+		if (keyword != null && !keyword.equals("")) {
+			contactUrl = Uri.withAppendedPath(
+					ContactsContract.Contacts.CONTENT_FILTER_URI, Uri.encode(keyword));
+		}
+		String[] columns = { ContactsContract.Contacts._ID,
+				ContactsContract.Contacts.DISPLAY_NAME };
+		String selection = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+	            + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+		String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+		String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
+		int[] to = {android.R.id.text1};
+		
+		Cursor c = resolver.query(contactUrl, columns, selection, null, sortOrder);
+		startManagingCursor(c);
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+				MainActivity.this, 
+				android.R.layout.simple_list_item_1, 
+				c, 
+				from, 
+				to, 
+				0);
+		list.setAdapter(adapter);
 	}
 
 	@Override
