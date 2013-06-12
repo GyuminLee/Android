d8651c20c16bf7ebcd218c32db3c4445bb702d33
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-06-12 07:50:32 +0000

    Loader

--- trunk/TestContentGetSample/src/com/example/testcontentgetsample/MainActivity.java
+++ trunk/TestContentGetSample/src/com/example/testcontentgetsample/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.testcontentgetsample;
 
-import android.app.Activity;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,10 +19,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements LoaderCallbacks<Cursor>{
 
 	EditText keywordView;
 	ListView list;
+	SimpleCursorAdapter mAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +37,20 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				String keyword = keywordView.getText().toString();
-				showList(keyword);
+				Bundle b = new Bundle();
+				b.putString("keyword", keyword);
+				getSupportLoaderManager().restartLoader(0, b, MainActivity.this);
 			}
 		});
+		
 		keywordView.addTextChangedListener(new TextWatcher() {
 			
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				showList(s.toString());
+//				showList(s.toString());
+				Bundle b = new Bundle();
+				b.putString("keyword", s.toString());
+				getSupportLoaderManager().restartLoader(0, b, MainActivity.this);
 			}
 			
 			@Override
@@ -53,12 +64,74 @@ public class MainActivity extends Activity {
 				
 			}
 		});
+		
+		String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
+		int[] to = {android.R.id.text1};
+		mAdapter = new SimpleCursorAdapter(
+				MainActivity.this, 
+				android.R.layout.simple_list_item_1, 
+				null, 
+				from, 
+				to, 
+				0);
+		list.setAdapter(mAdapter);
+		
+		getSupportLoaderManager().initLoader(0, null, this);
+		
 	}
 	
 	
 	private void showList(String keyword) {
-		ContentResolver resolver = getContentResolver();
+		new MyWorker().execute(keyword);
+	}
+	
+	class MyWorker extends AsyncTask<String, Integer, Cursor> {
+
+		@Override
+		protected Cursor doInBackground(String... keywords) {
+			String keyword = null;
+			if (keywords != null && keywords.length > 0) {
+				keyword = keywords[0];
+			}
+			ContentResolver resolver = getContentResolver();
+			
+			Uri contactUrl = ContactsContract.Contacts.CONTENT_URI;
+			if (keyword != null && !keyword.equals("")) {
+				contactUrl = Uri.withAppendedPath(
+						ContactsContract.Contacts.CONTENT_FILTER_URI, Uri.encode(keyword));
+			}
+			String[] columns = { ContactsContract.Contacts._ID,
+					ContactsContract.Contacts.DISPLAY_NAME };
+			String selection = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+		            + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+			String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+			
+			Cursor c = resolver.query(contactUrl, columns, selection, null, sortOrder);
+			return c;
+		}
 		
+		@Override
+		protected void onPostExecute(Cursor result) {
+			startManagingCursor(result);
+			mAdapter.swapCursor(result);
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int code, Bundle args) {
+		String keyword = null;
+		if (args != null) {
+			keyword = args.getString("keyword");
+		}
 		Uri contactUrl = ContactsContract.Contacts.CONTENT_URI;
 		if (keyword != null && !keyword.equals("")) {
 			contactUrl = Uri.withAppendedPath(
@@ -69,26 +142,20 @@ public class MainActivity extends Activity {
 		String selection = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
 	            + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
 		String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
-		String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
-		int[] to = {android.R.id.text1};
-		
-		Cursor c = resolver.query(contactUrl, columns, selection, null, sortOrder);
-		startManagingCursor(c);
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-				MainActivity.this, 
-				android.R.layout.simple_list_item_1, 
-				c, 
-				from, 
-				to, 
-				0);
-		list.setAdapter(adapter);
+				
+		return new CursorLoader(this, contactUrl, columns, selection, null, sortOrder);
 	}
 
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		mAdapter.swapCursor(cursor);
+	}
+
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		mAdapter.swapCursor(null);
 	}
 
 }
