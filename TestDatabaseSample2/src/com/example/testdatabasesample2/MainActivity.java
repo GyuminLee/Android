513c0cbda22870ade07b4adb626ab7628f140dfb
dongja94@gmail.com <dongja94@gmail.com@93695c4f-0603-dd79-8590-7915d6f047ac>
2013-06-12 05:37:13 +0000

    SimpleCursorAdapter

--- TestDatabaseSample2/src/com/example/testdatabasesample2/MainActivity.java
+++ TestDatabaseSample2/src/com/example/testdatabasesample2/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.testdatabasesample2;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.testdatabasesample2.DBConstant.PersonTable;
 
 public class MainActivity extends Activity {
 
@@ -53,6 +60,38 @@ public class MainActivity extends Activity {
 						ArrayAdapter<Person> adapter = new ArrayAdapter<Person>(MainActivity.this,
 								android.R.layout.simple_list_item_1, personlist);
 						list.setAdapter(adapter);
+					}
+				}, mHandler);
+			}
+		});
+		
+		btn = (Button)findViewById(R.id.getCursor);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				DatabaseManager.getInstance().getPersonCurosr(new DatabaseManager.OnQueryCompleteListener2() {
+					
+					@Override
+					public void onCompleted(Cursor cursor) {
+						String[] from = {PersonTable.NAME, PersonTable.ADDRESS};
+						int[] to = {R.id.name, R.id.address};
+						SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+								MainActivity.this, 
+								R.layout.item_layout, 
+								cursor, 
+								from, 
+								to, 
+								0);
+						list.setAdapter(adapter);
+						list.setOnItemClickListener(new OnItemClickListener() {
+
+							@Override
+							public void onItemClick(AdapterView<?> parent,
+									View view, int position, long id) {
+								Toast.makeText(MainActivity.this, "id : " + id, Toast.LENGTH_SHORT).show();
+							}
+						});
 					}
 				}, mHandler);
 			}
